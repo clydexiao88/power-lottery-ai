c2ff78f37a874a3fdf130f58a2a4e633a432f82b
clydexiao88 <131701866+clydexiao88@users.noreply.github.com>
2026-02-19 16:22:02 +0800

    Update update_lottery.py

--- update_lottery.py
+++ update_lottery.py
@@ -6,5 +6,6 @@ TARGET = "weli_latest.csv"
 df = pd.read_csv(SOURCE)
 
 df.to_csv(TARGET, index=False)
+df.to_csv("weli_latest.csv", index=False, encoding="utf-8-sig")
 
 print("✅ 已同步到主資料庫 weli_latest.csv")
